fb5998352642e881ada2accd6a2ddba55dd0f958
Phoenix-flame <alireza17010@gmail.com>
2023-01-28 12:25:20 +0330

    Some fixes

--- part2.py
+++ part2.py
@@ -7,85 +7,56 @@ import numpy as np
 
 print(gym.__version__)
 
-def render(env, idx, to_file=False):
-    if gym.__version__ == '0.21.0':
-        if to_file:
-            img = env.render(mode='rgb_array')
-            Image.fromarray(img).save(f'{idx}.png')
-        else:
-            env.render(mode='human')
-    else:
-        env.render()
-
-
-to_file = False
-
 if gym.__version__ == '0.21.0':
     env = gym.make('highway-v0', config={'observation': {"type": "Kinematics"}}) ## , config={'lane_change_reward': 0, 'observation': {"type": "TimeToCollision", 'horizon':10}} 
 else:
-    if to_file:
-    	env = gym.make('highway-v0', render_mode='rgb_array')
-    else:
-    	env = gym.make('highway-v0', render_mode='rgb_array')
+    env = gym.make('highway-v0', render_mode='rgb_array')
 
 env.metadata['render_fps'] = 30
-agent = dqnAgent(env=env, config={'log': True, 'memory_type':'per'}, ep_decay=0.005, id='_2')
+agent = dqnAgent(env=env, config={'log': True, 'memory_type':'per', 'scheduler_type':'exp'}, ep_decay=0.005, id='_2')
 agent.reset()
+
+## Load Learned Model
 agent.load(idx='_damn')
 
+
+## Train From Scratch
 # agent.learn(1000)
 # agent.save(idx='_test')
 
-from gym.wrappers import FilterObservation, FlattenObservation, TransformObservation
-env = TransformObservation(env, lambda x: x.flatten())
-from PIL import Image
+
+## Test agent
+from IPython import display as ipythondisplay
+from gym.wrappers import RecordVideo
+from pathlib import Path
+import base64
+
+def record_videos(env, video_folder="videos"):
+    wrapped = RecordVideo(env, video_folder=video_folder, episode_trigger=lambda e: True)
+    env.unwrapped.set_record_video_wrapper(wrapped)
+
+    return wrapped
+
+env = agent.env
 
 if gym.__version__ == '0.21.0':
-    if to_file:
-    	img = env.render(mode='rgb_array')
-    else:
-    	env.render(mode='human')
+    env.render(mode='human')
 else:
-    env.render()
-
+    env = record_videos(env)
 
 if gym.__version__ == '0.21.0':
     state = env.reset()
 else:
     state, _ = env.reset()
 
+
 state = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
-
-
-
-
-
-from IPython import display as ipythondisplay
-from gym.wrappers import RecordVideo
-from pathlib import Path
-import base64
-
-
-
-def record_videos(env, video_folder="videos"):
-    wrapped = RecordVideo(env, video_folder=video_folder, episode_trigger=lambda e: True)
-
-    # Capture intermediate frames
-    env.unwrapped.set_record_video_wrapper(wrapped)
-
-    return wrapped
-
-
-if gym.__version__ == '0.21.0':
-    render(env, 0, to_file)
-else:
-    env = record_videos(env)
 avg_reward = []
 for i in range(100):
     action = agent.policy(state, deterministic=True)
     if gym.__version__ == '0.21.0':
         observation, reward, terminated, info = env.step(action.item())
-        render(env, i + 1, to_file)
+        env.render(mode='human')
     else:
         observation, reward, terminated, _, info = env.step(action.item())
 
